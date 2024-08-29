5c950539b3b6a8e2f3f886e297be6a918b7c2610
Muhammad Rahim <129936056+MuhammadRahimST10043611@users.noreply.github.com>
2024-08-29 15:53:16 +0200

    Add project files.

---
+++ CLDV6212POEPART1/Models/CustomerProfile.cs
@@ -0,0 +1,25 @@
+using Azure.Data.Tables;
+using Azure;
+
+namespace CLDV6212POEPART1.Models
+{
+    public class CustomerProfile : ITableEntity
+    {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+
+        // Custom properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public CustomerProfile()
+        {
+            PartitionKey = "CustomerProfile";
+            RowKey = Guid.NewGuid().ToString();
+        }
+    }
+}
